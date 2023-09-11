86d6f046c7fda286fc83a545860c3de63059cc6c
gregoriocojr <gregoriocojr@gmail.com>
2023-09-11 15:29:48 +0800

    Prep 2 core req 1&2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your percentage grade? ");
+        string percentage = Console.ReadLine();
+        float grade = float.Parse(percentage);
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("Your letter grade is: A");
+        }
+
+        else if (grade >= 80)
+        {
+            Console.WriteLine("Your letter grade is: B");
+        }
+
+        else if (grade >= 70)
+        {
+            Console.WriteLine("Your letter grade is: C");
+        }
+
+        else if (grade >= 60)
+        {
+            Console.WriteLine("Your letter grade is: D");
+        }
+
+        else
+        {
+            Console.WriteLine("Your letter grade is: F");
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congartulations! You passed the course!");
+        }
+
+        else
+        {
+            Console.WriteLine("You may fell short this semester, but you can do better next time.");
+        }
     }
 }
