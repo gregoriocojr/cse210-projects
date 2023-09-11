5ee5cf833849f2fa6ac1a3a78e437b396f3b739f
gregoriocojr <gregoriocojr@gmail.com>
2023-09-11 15:44:06 +0800

    Prep 2 core req 3

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -7,31 +7,34 @@ class Program
         Console.Write("What is your percentage grade? ");
         string percentage = Console.ReadLine();
         float grade = float.Parse(percentage);
+        string letter;
 
         if (grade >= 90)
         {
-            Console.WriteLine("Your letter grade is: A");
+            letter = "A";
         }
 
         else if (grade >= 80)
         {
-            Console.WriteLine("Your letter grade is: B");
+            letter = "B";
         }
 
         else if (grade >= 70)
         {
-            Console.WriteLine("Your letter grade is: C");
+            letter = "C";
         }
 
         else if (grade >= 60)
         {
-            Console.WriteLine("Your letter grade is: D");
+            letter = "D";
         }
 
         else
         {
-            Console.WriteLine("Your letter grade is: F");
+            letter = "F";
         }
+
+        Console.WriteLine($"Yout letter grade is {letter}.");
 
         if (grade >= 70)
         {
