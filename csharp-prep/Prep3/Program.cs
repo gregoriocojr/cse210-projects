6bba08344071906f60db0e03430347ebd9bc285e
gregoriocojr <gregoriocojr@gmail.com>
2023-09-12 08:48:12 +0800

    Prep 3 step 1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write($"What is the magic number? ");
+        string number = Console.ReadLine();
+        int intNumber = int.Parse(number);
+
+        Console.Write("What is your guess? ");
+        string guess = Console.ReadLine();
+        int intGuess = int.Parse(guess);
+
+
+        if (intGuess > intNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+
+        else if (intGuess < intNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
     }
 }
