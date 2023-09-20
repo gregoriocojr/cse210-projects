d2937e73288a1f19b2c3f55272abc54697179134
gregoriocojr <gregoriocojr@gmail.com>
2023-09-20 21:36:41 +0800

    Design stage

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+public class Journal
+{
+    public List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry newEntry)
+    {
+        Console.WriteLine();
+    }
+
+    public void DisplayAll()
+    {
+        Console.WriteLine();
+    }
+
+    public void SaveToFile(string file)
+    {
+        Console.WriteLine();
+    }
+
+    public void LoadFromFile(string file)
+    {
+        Console.WriteLine();
+    }
+}
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Console.WriteLine("Welcome to the Journal Program");
+        Console.WriteLine("Please select one of the following choices:");
+        Console.WriteLine("1. Write\n2. Display\n3. Load\n4. Save\n5. Quit");
+        Console.Write("What would you like to do? ");
+        string response = Console.ReadLine();
+
+        do
+        {
+            if (response == "1")
+            {
+                ;
+            }
+
+            else if (response == "2")
+            {
+                ;
+            }
+
+            else if (response == "3")
+            {
+                ;
+            }
+
+            else if (response == "4")
+            {
+                ;
+            }
+
+            else if (response == "5")
+            {
+                ;
+            }
+        } while (false);
+            Console.WriteLine("Please select from the menu of options.");
     }
 }
+    //     _prompts.Add("Who was the most interesting person I interacted with today?");
+    //     _prompts.Add("What was the best part of my day?");
+    //     _prompts.Add("How did I see the hand of the Lord in my life today?");
+    //     _prompts.Add("What was the strongest emotion I felt today?");
+    //     _prompts.Add("If I had one thing I could do over today, what would it be?");
