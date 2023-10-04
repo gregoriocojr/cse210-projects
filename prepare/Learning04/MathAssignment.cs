c851696cf7debfa6510df4fc8d101cc0009c08ce
gregoriocojr <gregoriocojr@gmail.com>
2023-10-04 20:42:30 +0800

    prep 04

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,53 @@
+// using System;
+
+// public class MathAssignment : Assignment
+// {
+//     private string _textbookSection;
+//     private string _problem;
+
+//     public string GetTextbookSection()
+//     {
+//         return _textbookSection;
+//     }
+
+//     public void SetTextbookSection(string textbookSection)
+//     {
+//         _textbookSection = textbookSection;
+//     }
+
+//     public string GetProblem()
+//     {
+//         return _problem;
+//     }
+
+//     public void SetProblem(string problem)
+//     {
+//         _problem = problem;
+//     }
+
+//     public string GetHomeworkList()
+//     {
+//         return ($"Section {_textbookSection} Problems {_problem}");
+//     }
+// }
+
+public class MathAssignment : Assignment
+{
+    private string _textbookSection;
+    private string _problems;
+
+    // Notice the syntax here that the MathAssignment constructor has 4 parameters and then
+    // it passes 2 of them directly to the "base" constructor, which is the "Assignment" class constructor.
+    public MathAssignment(string studentName, string topic, string textbookSection, string problems)
+        : base(studentName, topic)
+    {
+        // Here we set the MathAssignment specific variables
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+
+    public string GetHomeworkList()
+    {
+        return $"Section {_textbookSection} Problems {_problems}";
+    }
+}
